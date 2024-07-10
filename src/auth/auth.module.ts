58c9b8f02fa0cc3f29b3dfa26fa7cda8186dc6f9
Dhruvi <connect.dhruvi@gmail.com>
2024-07-10 14:21:38 -0400

    -- Generating JWT token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,23 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
 import { User } from "./user.entity";
 import { LocalStrategy } from "./local.strategy";
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    providers: [LocalStrategy],
+    imports: [TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+        useFactory: () => ({
+            secret: process.env.AUTH_SECRET,
+            signOptions: {
+                expiresIn: '60m'
+            }
+        })
+    })],
+    providers: [LocalStrategy, AuthService],
     controllers: [AuthController]
 })
 
